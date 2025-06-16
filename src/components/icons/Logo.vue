7d6cc558ee539709305690ae6b8dc0b1e82782e6
晓尘 <user@example.com>
2025-06-16 18:46:17 +0800

    fix：Landing界面样式修改

--- src/components/icons/Logo.vue
+++ src/components/icons/Logo.vue
@@ -15,8 +15,6 @@
   justify-content: center;
   width: 100%;
   height: 100%;
-  user-select: none;
-  pointer-events: none;
 }
 
 .logo-image {
@@ -24,16 +22,5 @@
   height: 100%;
   object-fit: contain;
   filter: brightness(0.8);
-  user-select: none;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  pointer-events: none;
-  -webkit-user-drag: none;
-  -khtml-user-drag: none;
-  -moz-user-drag: none;
-  -o-user-drag: none;
-  user-drag: none;
-  -webkit-touch-callout: none;
 }
 </style> 
